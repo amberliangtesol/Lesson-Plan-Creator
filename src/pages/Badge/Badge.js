c3f6b1d4751afb299c061e1cf4b5c57c645221a7
amberliangtesol <amberliangtesol@gmail.com>
2023-04-15 21:24:57 +0800

    feat:badge redeem and batch render

--- src/pages/Badge/Badge.js
+++ src/pages/Badge/Badge.js
@@ -1,14 +1,110 @@
-import React from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components/macro";
-
+import { UserContext } from "../../UserInfoProvider";
+import {
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  query,
+  where,
+} from "firebase/firestore";
+import { db } from "../../utils/firebaseApp";
+import badge1 from "./badge1.png";
+import badge2 from "./badge2.png";
+import usedbadge1 from "./usedbadge1.png";
+import usedbadge2 from "./usedbadge2.png";
 
 function Badge() {
+  const { user, setUser } = useContext(UserContext);
+  const [collectedBadges, setCollectedBadges] = useState([]);
+  const [usedBadges, setUsedBadges] = useState([]);
+
+  useEffect(() => {
+    async function fetchUserData() {
+      if (user.name) return;
+
+      const docRef = doc(db, "users", user.account);
+      const docSnap = await getDoc(docRef);
+      if (docSnap.exists()) {
+        const userData = docSnap.data();
+        if (userData) {
+          // Add this condition to check if userData is defined
+          // Fetch class names
+          const classNames = await Promise.all(
+            userData.classes.map(async (classId) => {
+              const classDoc = await getDoc(doc(db, "classes", classId));
+              return classDoc.data() && classDoc.data().name;
+            })
+          );
+          setUser({
+            ...user,
+            image: userData.image,
+            name: userData.name,
+            classes: userData.classes,
+            classNames,
+          });
+        }
+      }
+    }
+
+    fetchUserData();
+  }, [user]);
+
+  useEffect(() => {
+    async function fetchBadgeData() {
+      if (!user.name) return;
+
+      const docRef = doc(db, "users", user.account);
+      const docSnap = await getDoc(docRef);
+      if (docSnap.exists()) {
+        const badgeData = docSnap.data().badge;
+        if (badgeData) {
+          setCollectedBadges(badgeData.collected || []);
+          setUsedBadges(badgeData.outdated || []);
+          console.log("badgeData", badgeData);
+        }
+      }
+    }
+
+    fetchBadgeData();
+  }, [user]);
+
+  const renderBadge = (badgeId, index) => {
+    switch (badgeId) {
+      case "badge1":
+        return <img key={`badge1-${index}`} src={badge1} alt="Badge 1" />;
+      case "badge2":
+        return <img key={`badge2-${index}`} src={badge2} alt="Badge 2" />;
+      case "usedbadge1":
+        return (
+          <img
+            key={`usedbadge1-${index}`}
+            src={usedbadge1}
+            alt="Used Badge 1"
+          />
+        );
+      case "usedbadge2":
+        return (
+          <img
+            key={`usedbadge2-${index}`}
+            src={usedbadge2}
+            alt="Used Badge 2"
+          />
+        );
+      default:
+        return null;
+    }
+  };
+
   return (
     <div>
       <h3>徽章搜集</h3>
       <Container>
         <Container1>
+          <ProfileImg imageURL={user.image}></ProfileImg>
+          <p>Hello {user.name}!</p>
           <BtnContainer>
             <Btn>
               <Link to="/StudentMain">課程主頁</Link>
@@ -21,7 +117,24 @@ function Badge() {
             </Btn>
           </BtnContainer>
         </Container1>
-        <Container2 style={{ paddingLeft: "50px" }}></Container2>
+
+        <div>
+          <Container2 style={{ paddingLeft: "50px" }}>
+            <h4>我的徽章</h4>
+            <BadgeContainer>
+              {collectedBadges.map((badgeId, index) =>
+                renderBadge(badgeId, index)
+              )}
+            </BadgeContainer>
+          </Container2>
+
+          <Container2 style={{ paddingLeft: "50px" }}>
+            <h4>兌換紀錄</h4>
+            <BadgeContainer>
+              {usedBadges.map((badgeId, index) => renderBadge(badgeId, index))}
+            </BadgeContainer>
+          </Container2>
+        </div>
       </Container>
     </div>
   );
@@ -54,12 +167,31 @@ const Container = styled.div`
 const Container1 = styled.div`
   display: flex;
   flex-direction: column;
+  align-items: center;
 `;
 
 const Container2 = styled.div`
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
 `;
 
+const ProfileImg = styled.div`
+  width: 150px;
+  height: 150px;
+  border: 1px solid black;
+  background-image: url(${(props) => props.imageURL});
+  background-size: cover;
+  background-position: center;
+  border-radius: 50%;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+`;
+
+const BadgeContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 8px;
+`;
 
 export default Badge;
--- src/pages/ManageBadge/ManageBadge.js
+++ src/pages/ManageBadge/ManageBadge.js
@@ -10,14 +10,17 @@ import {
   getDocs,
   query,
   where,
+  updateDoc,
 } from "firebase/firestore";
 import { db } from "../../utils/firebaseApp";
 
 function ManageBadge() {
   const { user, setUser } = useContext(UserContext);
   const [classNames, setClassNames] = useState([]);
-  console.log(user);
-  
+  const [classDetails, setClassDetails] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  // First useEffect for fetching and setting user data
   useEffect(() => {
     async function fetchUserData() {
       if (user.name) return;
@@ -32,21 +35,163 @@ function ManageBadge() {
           name: userData.name,
           classes: userData.classes,
         });
-
-        // Fetch class names
-        const classNames = await Promise.all(
-          userData.classes.map(async (classId) => {
-            const classDoc = await getDoc(doc(db, "classes", classId));
-            return classDoc.data().name;
-          })
-        );
-        setClassNames(classNames);
       }
     }
     fetchUserData();
+  }, [user.account]);
 
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [user]);
+  // Second useEffect for fetching and setting class details
+  useEffect(() => {
+    async function fetchClassDetails() {
+      if (!user.classes) return;
+
+      const classDetails = await Promise.all(
+        user.classes.map(async (classId) => {
+          const classDoc = await getDoc(doc(db, "classes", classId));
+          const classData = classDoc.data();
+
+          if (!classData) {
+            console.warn(`Class data not found for class ID: ${classId}`);
+            return null;
+          }
+
+          return {
+            name: classData.name,
+            id: classData.id,
+          };
+        })
+      );
+      setClassDetails(classDetails.filter(Boolean));
+      setIsLoading(false);
+    }
+    fetchClassDetails();
+  }, [user.classes]);
+
+  const ContainerContent = ({ description }) => {
+    const [selectedClass, setSelectedClass] = useState("");
+    const [students, setStudents] = useState([]);
+    const [selectedStudent, setSelectedStudent] = useState("");
+    const [selectedBadge, setSelectedBadge] = useState("");
+
+    useEffect(() => {
+      async function fetchStudents() {
+        if (!selectedClass) {
+          setStudents([]);
+          setSelectedStudent("");
+          setSelectedBadge("");
+          return;
+        }
+
+        const classesCollectionRef = collection(db, "classes");
+        const querySnapshot = await getDocs(
+          query(classesCollectionRef, where("name", "==", selectedClass))
+        );
+
+        if (!querySnapshot.empty) {
+          const classData = querySnapshot.docs[0].data();
+          setStudents(classData.students);
+        } else {
+          setStudents([]);
+        }
+      }
+
+      fetchStudents();
+    }, [selectedClass]);
+
+    const redeemBadge = async (student, badge) => {
+      const badgeMapping = {
+        準時完成作業: "badge1",
+        挑戰打怪成功: "badge2",
+      };
+    
+      const badgeCode = badgeMapping[badge];
+    
+      const studentRef = doc(db, "users", student);
+      const studentSnap = await getDoc(studentRef);
+    
+      if (studentSnap.exists()) {
+        const studentBadgeData = studentSnap.data().badge;
+        const collected = Array.isArray(studentBadgeData.collected)
+          ? studentBadgeData.collected
+          : [];
+        const outdated = Array.isArray(studentBadgeData.outdated)
+          ? studentBadgeData.outdated
+          : [];
+    
+        if (collected.includes(badgeCode)) {
+          const updatedCollected = collected.filter(
+            (item) => item !== badgeCode
+          );
+          const updatedOutdated = [...outdated, `used${badgeCode}`];
+    
+          await updateDoc(studentRef, {
+            badge: {
+              collected: updatedCollected,
+              outdated: updatedOutdated,
+            },
+          });
+    
+          alert("徽章已兌換");
+        } else {
+          alert("該學生無該徽章");
+        }
+      } else {
+        alert("學生資料不存在");
+      }
+    };
+    
+
+    return (
+      <Container2 style={{ paddingLeft: "50px" }}>
+        <p>選擇班級</p>
+        <select
+          value={selectedClass}
+          onChange={(e) => {
+            setSelectedClass(e.target.value);
+            setSelectedStudent("");
+            setSelectedBadge("");
+          }}
+          style={{ width: "150px" }}
+        >
+          <option value="">選擇班級</option>
+          {classDetails.map((classItem, index) => (
+            <option key={index} value={classItem.name}>
+              {classItem.name}
+            </option>
+          ))}
+        </select>
+        <p>選擇學生</p>
+        <select
+          value={selectedStudent}
+          onChange={(e) => setSelectedStudent(e.target.value)}
+          style={{ width: "150px" }}
+        >
+          <option value="">選擇學生</option>
+          {students.map((student, index) => (
+            <option key={index} value={student}>
+              {student}
+            </option>
+          ))}
+        </select>
+        <p>兌換徽章</p>
+        <select
+          value={selectedBadge}
+          onChange={(e) => setSelectedBadge(e.target.value)}
+          style={{ width: "150px" }}
+        >
+          <option value="">選擇徽章</option>
+          <option value="準時完成作業">準時完成作業</option>
+          <option value="挑戰打怪成功">挑戰打怪成功</option>
+        </select>
+        <Btn
+          style={{ marginBottom: "30px" }}
+          onClick={() => redeemBadge(selectedStudent, selectedBadge)}
+        >
+          確認兌換
+        </Btn>
+      </Container2>
+    );
+  };
 
   return (
     <div>
@@ -70,35 +215,7 @@ function ManageBadge() {
             </Btn>
           </BtnContainer>
         </Container1>
-        <Container2 style={{ paddingLeft: "50px" }}>
-          <Badge />
-          <p>連續答對三題</p>
-          <p>選擇班級</p>
-          <input></input>
-          <p>選擇學生</p>
-          <input></input>
-          <Btn style={{ marginBottom: "30px" }}>確認兌換</Btn>
-        </Container2>
-
-        <Container2 style={{ paddingLeft: "50px" }}>
-          <Badge />
-          <p>準時完成作業</p>
-          <p>選擇班級</p>
-          <input></input>
-          <p>選擇學生</p>
-          <input></input>
-          <Btn style={{ marginBottom: "30px" }}>確認兌換</Btn>
-        </Container2>
-
-        <Container2 style={{ paddingLeft: "50px" }}>
-          <Badge />
-          <p>挑戰打怪成功</p>
-          <p>選擇班級</p>
-          <input></input>
-          <p>選擇學生</p>
-          <input></input>
-          <Btn style={{ marginBottom: "30px" }}>確認兌換</Btn>
-        </Container2>
+        <ContainerContent />
       </Container>
     </div>
   );
@@ -137,13 +254,6 @@ const Container1 = styled.div`
 const Container2 = styled.div`
   display: flex;
   flex-direction: column;
-`;
-
-const Badge = styled.div`
-  width: 86px;
-  height: 86px;
-  background-image: url(${badge});
-  cursor: pointer;
 `;
 
 const ProfileImg = styled.div`
